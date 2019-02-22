144006055db89ff6658a05918b479d70d24082f4
liushuyu <liushuyu011@gmail.com>
2019-02-22 15:09:31 -0700

    az-pipeline: use Azure Pipeline for CI...
    ... breaks Travis CI b/c environments are very different

--- .travis/linux-frozen/docker.sh
+++ .travis/linux-frozen/docker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -ex
 
-cd /citra
-
 mkdir build && cd build
 cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DCITRA_ENABLE_COMPATIBILITY_REPORTING=${ENABLE_COMPATIBILITY_REPORTING:-"OFF"} -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DUSE_DISCORD_PRESENCE=ON
 ninja
