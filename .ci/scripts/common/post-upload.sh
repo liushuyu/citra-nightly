03411633929ba8de91ad419515961c6610ddf7e4
liushuyu <liushuyu011@gmail.com>
2020-08-27 21:18:30 -0600

    ci: post-upload: make the script more verbose

--- .ci/scripts/common/post-upload.sh
+++ .ci/scripts/common/post-upload.sh
@@ -3,19 +3,19 @@
 RELEASE_NAME="${RELEASE_NAME:-head}"
 
 # Copy documentation
-cp license.txt "$DIR_NAME"
-cp README.md "$DIR_NAME"
+cp -nv license.txt "$DIR_NAME"
+cp -nv README.md "$DIR_NAME"
 
 tar -cJvf "${REV_NAME}-source.tar.xz" src externals CMakeLists.txt README.md license.txt
-cp "${REV_NAME}-source.tar.xz" "$DIR_NAME"
+cp -nv "${REV_NAME}-source.tar.xz" "$DIR_NAME"
 
 tar $COMPRESSION_FLAGS "$ARCHIVE_NAME" "$DIR_NAME"
 
-mv "$DIR_NAME" $RELEASE_NAME
-mv "${REV_NAME}-source.tar.xz" $RELEASE_NAME
+mv -v "$DIR_NAME" $RELEASE_NAME
+mv -v "${REV_NAME}-source.tar.xz" $RELEASE_NAME
 
 7z a "$REV_NAME.7z" $RELEASE_NAME
 
 # move the compiled archive into the artifacts directory to be uploaded by travis releases
-mv "$ARCHIVE_NAME" artifacts/
-mv "$REV_NAME.7z" artifacts/
+mv -v "$ARCHIVE_NAME" artifacts/
+mv -v "$REV_NAME.7z" artifacts/
